d3ad545238ac506aa4b2aeb20d9c40fed8a9db5b
akme <users.noreply.github.com>
2019-12-29 21:34:32 +0700

    * fix

--- transactions.sh
+++ transactions.sh
@@ -7,10 +7,12 @@ while true
         block_counter=$(cat blockid)
         transactions=$(cat transactions)
         block=$(/path/to/lite-client/lite-client -C /home/wallet/ton-lite-client-test1.config.json -v 0 -c "last" 2> >(tail -n 1|awk {'print $8'}))
-        if [ "$block_counter" != "$block" ]
+        if [ -n "$block" ] && [ "$block_counter" != "$block" ]
         then
             trans_num=$(/path/to/lite-client/lite-client -C /home/wallet/ton-lite-client-test1.config.json -v 0 -c "listblocktrans $block 100" 2> >(tail -n 2| head -n 1|awk {'print $2'}|sed -e 's/#//g' -e 's/://g'))
             echo $(("$transactions"+"$trans_num")) > transactions
+            echo "${block}" > blockid
         fi
         sleep 3
 done
+
