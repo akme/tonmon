384a9b122a568a0d2e675e58f4e82fb69c951b0f
akme <akme@users.noreply.github.com>
2020-04-08 20:28:48 +0300

    * fix output redirect

--- transactions.sh
+++ transactions.sh
@@ -6,10 +6,10 @@ while true
     do 
         block_counter=$(cat blockid)
         transactions=$(cat transactions)
-        block=$(/path/to/lite-client/lite-client -C /home/wallet/ton-lite-client-test1.config.json -v 0 -c "last" 2> >(tail -n 1|awk {'print $8'}))
+        block=$(/path/to/lite-client/lite-client -C /home/wallet/ton-lite-client-test1.config.json -v 0 -c "last" |tail -n 1|awk {'print $8'})
         if [ -n "$block" ] && [ "$block_counter" != "$block" ]
         then
-            trans_num=$(/path/to/lite-client/lite-client -C /home/wallet/ton-lite-client-test1.config.json -v 0 -c "listblocktrans $block 100" 2> >(tail -n 2| head -n 1|awk {'print $2'}|sed -e 's/#//g' -e 's/://g'))
+            trans_num=$(/path/to/lite-client/lite-client -C /home/wallet/ton-lite-client-test1.config.json -v 0 -c "listblocktrans $block 100" |tail -n 2| head -n 1|awk {'print $2'}|sed -e 's/#//g' -e 's/://g')
             echo $(("$transactions"+"$trans_num")) > transactions
             echo "${block}" > blockid
         fi
